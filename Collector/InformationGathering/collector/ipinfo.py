5470ca1f2f77537295ba3ea9310cc272ee715247
root <root@kali.kali>
2025-03-18 16:01:37 -0400

    add to ipinfo file

--- Collector/InformationGathering/collector/ipinfo.py
+++ Collector/InformationGathering/collector/ipinfo.py
@@ -1,1 +1,19 @@
 #Top li te keve
+
+#JEHIR
+import socket 
+import request
+
+def get_ip_info(target):
+    try:
+        ip_address = socket.gethostbyname(target)
+        response = request.get(f"https://ipinfo.io/{ip_address}/json")
+        data = response.json()
+
+        ip_info ={
+
+            "IP ADDRESS": data.get("ip", N/A),
+            "HOSTNAME": target,
+            "CITY": data.get("city", N/A)
+        }
+        return ip_info
