39a3f160a8df20cbfc4a50887d52cfd7664e04a8
root <root@kali.kali>
2025-03-22 16:36:43 -0400

    add update and delete items to views, create new file  where all functions are placed inside a single class called itemviews, edit the urls and add new path, delete the the old paths, edit the models and put 2 new lines to define name and id, edit the views to only handle general views and import item related views from itemviews, to keep views clean and simple

--- Collector/InformationGathering/models.py
+++ Collector/InformationGathering/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 
-class Item:
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
     def __init__(self, id, name):
         self.id = id
         self.name = name
-
-
 
 items = [
     Item(1, "Item 1"),
     Item(2, "Item 2"),
     Item(3, "Item 3"),
+
+    
 ]
+
+
+ 
--- Collector/InformationGathering/urls.py
+++ Collector/InformationGathering/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import app_name_view
-from .views import get_item, add_item
+from .itemviews import ItemViews
 
 urlpatterns = [
     path('', app_name_view, name='app_name'),
-    path('get-item/', get_item, name='get_item'),
-    path('add-item/', add_item, name='add_item'),
+    path('items/', ItemViews.as_view(), name='items'),
 ]
+
