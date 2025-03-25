a7a30a2632c27a7c5336d7e1bb6ed02847e29c11
Baran <baran@baran.baran>
2025-03-25 14:24:32 -0400

    restore user managment

--- Collector/InformationGathering/urls.py
+++ Collector/InformationGathering/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import app_name_view
 from .targetmanagement import TargetManagement
+from .UserManagement import UserManagement
 
 urlpatterns = [
     path('', app_name_view, name='app_name'),
     path('target/', TargetManagement.as_view(), name='target_management'),
+    path('users/', UserManagement.as_view(), name='user_management'),
 ]
